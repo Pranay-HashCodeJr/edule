f02bcbd7e264fa9d0b44abe0a41747a01476eba0
Anubhav <anubhavjhawar08@gmail.com>
2021-08-19 18:43:04 +0530

    files added to anubhav brancg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import logo from './logo.svg';
 import './App.css';
-
+import Header from './Component/Header';
 function App() {
   return (
     <div className="App">
-      EDULE: E-Learning
+      <Header/>
     </div>
   );
 }
---
+++ src/Component/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Header.css'
+export default function Header() {
+    return (
+        <div>
+            <div className="Head">
+               <span className="container-fluid">
+                   <img src= {"https://jasonjun-envato.github.io/envato-design-resources/logos/envato-market-white.png"} height="18px" width="152px"></img>
+                   <button type="button" class="bten1 btn-xs btn-success">Buy Now</button>
+              </span>
+              </div>   
+              <div className="Head2">
+              <div class="container">
+    <div class="row">
+      <div class="col-md-3"><p className="Head2sub container">All courses 28% off for <em>Liberian people's</em></p></div>
+      <div class="col-md-3">
+      <div class="row">
+      <div class="col-md-1"><i class="lni lni-phone"></i></div>
+      <div class="col-md-4"><p>(970) 262-1413</p></div>
+     </div>
+     </div>
+      <div class="col-md-3">
+      <div class="row">
+        <div class="col-md-1"><i class="lni lni-inbox"></i></div>
+      <div class="col-md-4"><p>address@gmail.com</p></div>
+     </div>
+     </div>
+     <div class="col-md-3">
+      <div class="row">
+        <div class="col-md-1"><i class="lni lni-facebook"></i></div>
+      <div class="col-md-1"><i class="lni lni-twitter"></i></div>
+        <div class="col-md-1"><i class="lni lni-linkedin"></i></div>
+      <div class="col-md-1"><i class="lni lni-instagram-original"></i></div>
+     </div>
+     </div>
+     </div>
+</div>
+</div>
+ </div>
+    )
+}
